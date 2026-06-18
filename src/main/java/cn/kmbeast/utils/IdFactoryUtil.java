0f217bd4cd51cbc0dda9941d0fbafe620aa20041
sxu404 <1838714197@qq.com>
2026-06-18 11:04:13 +0800

    管6.18

--- src/main/java/cn/kmbeast/utils/IdFactoryUtil.java
+++
@@ -1,9 +0,0 @@
-package cn.kmbeast.utils;
-
-import java.util.UUID;
-
-public class IdFactoryUtil {
-    public static String getFileId() {
-        return UUID.randomUUID().toString().substring(1, 8);
-    }
-}
